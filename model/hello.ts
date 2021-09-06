c34304d14a9ac19c0774ad4a06b900d067b8e50c
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2021-09-06 22:12:40 +0900

    update

--- model/hello.ts
+++ model/hello.ts
@@ -6,15 +6,20 @@ export {
 }
 
 async function aaa(){
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        // docker 内では下の様にする。
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      });
     const page = await browser.newPage();
 
     // 環境変数screenshot_dirが定義されていたら、それを使う。それ以外はフォルダ配下のディレクトリを使う。
     const screenshot_dir = process.env.SCREENSHOT_DIR ?? 'screenshot'
 
-    await Promise.all([
-        page.goto('https://example.com'),
-        page.screenshot({ path: path.join(screenshot_dir,'example.png')}),
-        browser.close()
-    ])
+    await page.goto('https://example.com'),
+    await page.screenshot({ path: path.join(screenshot_dir,'example.png')}),
+    await browser.close()
+
 }
