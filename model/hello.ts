dde9cc4c771a669fd9bb17a3078f0f53c5887ddd
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2021-09-06 14:19:23 +0900

    update

--- model/hello.ts
+++ model/hello.ts
@@ -1,14 +1,19 @@
+import path from 'path/posix';
 import puppeteer from 'puppeteer'
 
 export {
-    aaa
+    aaa,
 }
 
 async function aaa(){
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
+    // 環境変数screenshot_dirが定義されていたら、それを使う。それ以外はフォルダ配下のディレクトリを使う。
+    let screenshot_dir = process.env.SCREENSHOT_DIR ?? 'screenshot'
+
     await page.goto('https://example.com');
-    await page.screenshot({ path: 'example.png' });
+    await page.screenshot({ path: path.join(screenshot_dir,'example.png')  });
 
     await browser.close();
 }
