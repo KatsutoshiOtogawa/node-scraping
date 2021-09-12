5c286e6d17a4cb22600d6fdc0e9c2e0cb7732499
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2021-09-12 10:05:40 +0900

    update

--- index.ts
+++ index.ts
@@ -1,40 +1,13 @@
 import 'reflect-metadata'
-import { Model } from './model'
-import { Command } from 'commander'
 import { db } from './lib/db'
 import { logger } from './lib/log'
-import { ArgsForModel } from './lib/config'
+import { Model } from './model'
 
 // err時のstack traceを表示させるために必要。
 import sourceMapSupport from 'source-map-support'
 sourceMapSupport.install()
 
-const program = new Command()
-
-program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza')
-  // .option('-m, --model-name <modelName>', '実行するモデル名です。')
-  // .option('-m, --model-name <modelName>', '実行するモデル名です。')
-
-program.parse(process.argv)
-
-const options = program.opts()
-
-// console.log(options)
-
-if (options.debug) logger.info(options)
-logger.info('pizza details:')
-if (options.small) logger.info('- small pizza size')
-if (options.pizzaType) console.log(`- ${options.pizzaType}`)
-
-// options.modelName
-
-const argsForModel = new ArgsForModel('Amazon', 'search', logger)
-
-// 処理呼び出し。
-Model.invoke(argsForModel)
+Model.command()
 
 db.pool.connect((err, client, release) => {
   if (err) {
--- model/index.ts
+++ model/index.ts
@@ -2,6 +2,45 @@
 // model配下の各処理へのエントリーポイントです。
 import { Amazon } from './amazon'
 import { ArgsForModel } from '../lib/config'
+// import { Model } from './model'
+import { Command } from 'commander'
+import { logger } from '../lib/log'
+
+/**
+ * 各処理を外部から呼び出すための処理です。
+ */
+function command () {
+  // ここから別の関数に分ける。
+  const program = new Command()
+
+  program
+    .option('-d, --debug', 'output extra debugging')
+    .option('-s, --small', 'small pizza size')
+    .option('-p, --pizza-type <type>', 'flavour of pizza')
+    .option('-m, --model-name <modelName>', '実行するモデル名です。')
+    .option('-f, --fun-name <funcName>', '実行する関数名です。')
+
+  program.parse(process.argv)
+
+  const options = program.opts()
+
+  // console.log(options)
+
+  if (options.debug) logger.info(options)
+  logger.info('pizza details:')
+  if (options.small) logger.info('- small pizza size')
+  if (options.pizzaType) console.log(`- ${options.pizzaType}`)
+
+  const modelName:string = options.modelName ?? ''
+  const funcName:string = options.funcName ?? ''
+
+  // modelName,funcNameがちゃんと渡されているなら処理を呼び出す。
+  if (modelName && funcName) {
+    const argsForModel = new ArgsForModel(modelName, funcName, logger)
+    // 処理呼び出し。
+    Model.invoke(argsForModel)
+  }
+}
 
 /**
  * 各モデルへのエントリーポイントです。
@@ -20,7 +59,8 @@ function invoke (argsForModel: ArgsForModel) {
 }
 
 const Model = {
-  invoke: invoke
+  invoke: invoke,
+  command: command
 }
 
 export {
